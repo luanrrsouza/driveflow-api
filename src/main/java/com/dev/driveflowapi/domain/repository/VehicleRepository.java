6a465a7a2f7a409832df80807bd3fc71f5d65469
luanrrsouza <luanrrdsouza@gmail.com>
2026-09-17 14:24:47 -0300

    feat: implementa contratos de persistência do domínio.

---
+++ src/main/java/com/dev/driveflowapi/domain/repository/VehicleRepository.java
@@ -0,0 +1,18 @@
+package com.dev.driveflowapi.domain.repository;
+
+import com.dev.driveflowapi.domain.model.Vehicle;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VehicleRepository {
+
+    Vehicle save(Vehicle vehicle);
+
+    Optional<Vehicle> findById(UUID id);
+
+    List<Vehicle> findAll();
+
+    void deleteById(UUID id);
+}
