3890cb239900cdf58a9fa249ee7b28773e905340
luanrrsouza <luanrrdsouza@gmail.com>
2026-09-18 10:47:34 -0300

    feat: implementa mapeadores da camada de aplicação

--- src/main/java/com/dev/driveflowapi/application/mapper/VehicleMapper.java
+++ src/main/java/com/dev/driveflowapi/application/mapper/VehicleMapper.java
@@ -1,4 +1,23 @@
 package com.dev.driveflowapi.application.mapper;
 
-public class VehicleMapper {
+import com.dev.driveflowapi.application.dto.input.vehicle.CreateVehicleInput;
+import com.dev.driveflowapi.application.dto.output.vehicle.VehicleOutput;
+import com.dev.driveflowapi.domain.model.Dealer;
+import com.dev.driveflowapi.domain.model.Vehicle;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface VehicleMapper {
+
+    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())")
+    @Mapping(target = "dealer", source = "dealer")
+    Vehicle toDomain(
+            CreateVehicleInput input,
+            Dealer dealer
+    );
+
+    @Mapping(target = "dealerId", source = "dealer.id")
+    @Mapping(target = "dealerName", source = "dealer.corporateName")
+    VehicleOutput toOutput(Vehicle vehicle);
 }
